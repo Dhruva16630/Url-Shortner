48c7d391875765a4d4f39185192dddcf164755eb
Dhruva16630 <dhruva6kumar1@gmail.com>
2025-08-19 21:08:40 +0530

    Correct_Backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import {nanoid} from "nanoid"
 import dotenv from "dotenv"
 import connectDB from "./src/config/monogo.config.js"
 import short_url from "./src/routes/short_url.route.js"
@@ -16,8 +15,8 @@ dotenv.config("./.env")
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // your React app
-    credentials: true // 👈 this allows cookies to be sent
+    origin: 'http://localhost:5173', 
+    credentials: true 
 }));
 
 app.use(express.json())
